e5b746f51d718cc12514555fd30ad99a612b0779
Nakia Matthews <nmatthews@solcyber.com>
2021-12-01 12:59:55 -0500

    final code

--- day-01/Day1.2.py
+++ day-01/Day1.2.py
@@ -2,10 +2,13 @@
 input = open("input1.txt", "r")
 input_list_raw = list(input.readlines())
 input_list = []
-increases = 0
+windows = []
 for x in input_list_raw:
     input_list.append(int(x.strip('\n')))
-for y, i in enumerate(input_list[:-1]):
-    if input_list[y + 1] > i:
+for index, num in enumerate(input_list[:-2]):
+    windows.append(num + input_list[index + 1] + input_list[index + 2])
+increases = 0
+for y, i in enumerate(windows[:-1]):
+    if windows[y + 1] > i:
         increases += 1
 print (increases)
