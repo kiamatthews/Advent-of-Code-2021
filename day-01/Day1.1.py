dc1966b44d6cb00a7dc8e75e3b8fc063318d2ac0
Nakia Matthews <nmatthews@solcyber.com>
2021-12-01 12:32:41 -0500

    answer obtained

--- day-01/Day1.1.py
+++ day-01/Day1.1.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-
-input = open("input.txt", "r")
-input_list = input.read()
+input = open("input1.txt", "r")
+input_list_raw = list(input.readlines())
+input_list = []
 counter = 0
-
-start = input_list[int(counter)]
-
-for x in input_list:
-    
+increases = 0
+for x in input_list_raw:
+    input_list.append(int(x.strip('\n')))
+for x in input_list[:-1]:
+    if input_list[int(counter) + 1] > x:
+        increases += 1
+    counter +=1
+print (increases)
