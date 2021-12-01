d4cae1228d34ab027391fa9cf8b121164167cfcc
Kia Matthews <kiamatthews@gmail.com>
2021-12-01 16:45:28 -0500

    deleted files

--- day-01/shorter.py
+++
@@ -1,9 +0,0 @@
-input_list = []
-input = open("input1.txt", "r")
-for line in input:
-    input_list.append(int(line.strip()))
-increases = 0
-for y, i in enumerate(input_list[:-1]):
-    if input_list[y + 1] > i:
-        increases += 1
-print (increases)
